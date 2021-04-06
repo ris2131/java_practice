7472e51884b50800b002da1b968a4485a1046ba2
ris2131 <inseok9876@gmail.com>
2021-04-06 18:58:21 +0900

    second first commit

---
+++ javaTutorials/src/org/opentutorials/javatutorials/numberstring/String.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		//String ryu = "류인석"; //error 아마 class 이름이String 이라 안되는듯.....
+		System.out.println("류인석");//string
+		System.out.println('류');//char
+		//System.out.println('인석');//error (문자열이 작은따옴표로 표기)
+		System.out.println("류"+" dlstjr");//operator '+'
+		System.out.println("류"=="류");//operator '=='
+	}
+
+}
